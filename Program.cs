0f63838fb6ec0e537f44136a613019640903184b
Landon Cassman <Landon.Cassman@gmail.com>
2022-06-08 11:51:46 -0400

    Created Dinosaur class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Dinosaur
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public string Diet { get; set; }
+        public DateTime WhenAcquired { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+        public string DisplayDinosaur()
+        {
+            return $"We have a {Name} that is a {Diet} that we  {WhenAcquired}. It weighs {Weight} and is enclosure {EnclosureNumber}";
+        }
+
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
